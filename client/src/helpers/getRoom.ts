48d33f3350eb02f7c7dfff3bd0ad3882785d72aa
jhonywalkeer <mak.jhon@hotmail.com>
2022-07-21 23:29:45 -0300

    feat: created getRoom function

---
+++ client/src/helpers/getRoom.ts
@@ -0,0 +1,17 @@
+export const whiteboardRoomIds = new Set<string>()
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const charactersLength = characters.length
+
+export function getRoomId() {
+  let result = ''
+  for (let i = 0; i < 12; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  if (!whiteboardRoomIds.has(result)) {
+    whiteboardRoomIds.add(result)
+    return result
+  } else {
+    console.log('roomId exists, remaking another one.')
+    getRoomId()
+  }
+}
