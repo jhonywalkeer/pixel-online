b6515521c7c0aa9106c2738d2df31fdbfe6c3173
jhonywalkeer <mak.jhon@hotmail.com>
2022-07-21 22:23:01 -0300

    chore: configuring linters for correct code formatting

---
+++ client/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: { 'prettier/prettier': 'error' },
+}
