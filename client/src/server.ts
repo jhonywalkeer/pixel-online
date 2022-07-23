ea99c06c2dd7e009c4d1952b853fc8bd3009c376
jhonywalkeer <mak.jhon@hotmail.com>
2022-07-23 20:59:30 -0300

    chore: configure server appplication

--- client/src/server.ts
+++ client/src/server.ts
@@ -1,6 +1,8 @@
 import http from 'http'
 import express from 'express'
 import cors from 'cors'
+import pino from 'pino'
+import logger from './utils/functions/logger.js'
 import { Server, LobbyRoom } from 'colyseus'
 import { monitor } from '@colyseus/monitor'
 import { Room } from './utils/enums/Rooms'
@@ -12,3 +14,6 @@ const gameServer = new Server({ server })
 
 app.user(cors())
 app.use(express.json())
+
+gameServer.define(Room.LOBBY, LobbyRoom)
+// gameServer.define(Room.PUBLIC)
