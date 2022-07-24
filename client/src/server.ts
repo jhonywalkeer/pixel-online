96b075d930b32c2d97a32d766dd1c8b6fcd453b6
jhonywalkeer <mak.jhon@hotmail.com>
2022-07-23 22:19:56 -0300

    chore: edit server

--- client/src/server.ts
+++ client/src/server.ts
@@ -1,19 +1,30 @@
 import http from 'http'
 import express from 'express'
 import cors from 'cors'
-import pino from 'pino'
 import logger from './utils/functions/logger.js'
 import { Server, LobbyRoom } from 'colyseus'
 import { monitor } from '@colyseus/monitor'
-import { Room } from './utils/enums/Rooms'
+import { RoomEnum } from './utils/enums/Rooms'
+import { Pixel } from './controllers/Environment'
 
 const port = Number(process.env.PORT || 2567)
 const app = express()
 const server = http.createServer(app)
 const gameServer = new Server({ server })
 
-app.user(cors())
+app.use(cors())
 app.use(express.json())
 
-gameServer.define(Room.LOBBY, LobbyRoom)
-// gameServer.define(Room.PUBLIC)
+gameServer.define(RoomEnum.LOBBY, LobbyRoom)
+gameServer.define(RoomEnum.PUBLIC, Pixel, {
+  name: 'Public Lobby',
+  description: 'For making friends and familiarizing yourself with the controls',
+  password: null,
+  autoDispose: false,
+})
+gameServer.define(RoomEnum.CUSTOM, Pixel).enableRealtimeListing()
+
+app.use('/colyseus', monitor())
+
+gameServer.listen(port)
+logger.info(`Listening on ws://localhost:${port}`)
